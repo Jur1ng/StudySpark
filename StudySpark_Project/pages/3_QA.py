dc1c13b5a0ebad017ff23d22d573d99436f38281
Jur1ng <iuriisheiman@gmail.com>
2025-12-20 20:14:31 +0000

    Add files via upload

--- StudySpark_Project/pages/3_QA.py
+++ StudySpark_Project/pages/3_QA.py
@@ -1,11 +1,39 @@
 import streamlit as st
+from services.ai_service import AIService
 
 st.title("Questions & Answers")
 
-question = st.text_input("Ask a question about the document:")
+#question = st.text_input("Ask a question about the document:")
 
-if st.button("Get Answer"):
-    if question:
-        st.success("Answer will be generated here.")
-    else:
-        st.warning("Please enter a question.")
+#if st.button("Get Answer"):
+#    if question:
+#        st.success("Answer will be generated here.")
+#    else:
+#        st.warning("Please enter a question.")
+
+
+document_text = st.session_state.pdf_text
+
+# Maintain conversation history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if user_question := st.chat_input("Ask a question about the document:"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(user_question)
+    # Add user message to chat history
+    st.session_state.chat_history.append({"role": "user", "content": user_question})
+
+    response = st.session_state.ai_service.question_answer(document_text, user_question)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+    # Add assistant response to chat history
+    st.session_state.chat_history.append({"role": "assistant", "content": response})
