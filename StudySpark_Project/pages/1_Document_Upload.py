d51d500e344ed3cbdebec5118374cf50a3239a92
Jur1ng <iuriisheiman@gmail.com>
2025-12-20 16:44:32 +0000

    Add files via upload

--- StudySpark_Project/pages/1_Document_Upload.py
+++ StudySpark_Project/pages/1_Document_Upload.py
@@ -1,12 +1,8 @@
 import streamlit as st
 from dotenv import load_dotenv
-
-from services.AIService import AIService
+from services.ai_service import AIService
 
 load_dotenv()
-
-if 'AIService' not in st.session_state:
-    st.session_state.AIService = AIService()
 
 st.title("Document Upload")
 
@@ -19,8 +15,7 @@ uploaded_file = st.file_uploader(
 if uploaded_file:
     pdf_bytes = uploaded_file.read()
     with st.spinner("Processing pdf..."):
-        # Call our service (all logic is in the service layer!)
-        result = st.session_state.AIService.process_pdf(pdf_bytes)
+        result = st.session_state.ai_service.process_pdf(pdf_bytes)
 
         # Store in session state for chat
         st.session_state.current_ticket = result
