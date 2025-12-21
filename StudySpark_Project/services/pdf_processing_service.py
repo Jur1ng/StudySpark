97b8d7626a91b315efdaae943a7897444c9a0d55
Jur1ng <iuriisheiman@gmail.com>
2025-12-21 20:41:40 +0000

    Update pdf_processing_service.py

--- StudySpark_Project/services/pdf_processing_service.py
+++ StudySpark_Project/services/pdf_processing_service.py
@@ -1,55 +1,17 @@
 from io import BytesIO
-from PyPDF2 import PdfReader
-from services.ai_service import AIService
-import streamlit as st
-import fitz
+from pypdf import PdfReader
 
+def process_pdf(pdf_bytes: bytes) -> str:
 
-def extract_images_from_pdf(pdf_bytes):
-    images = []
-    doc = fitz.open(stream=pdf_bytes, filetype="pdf")
-
-    for page_index, page in enumerate(doc):
-        for img in page.get_images(full=True):
-            xref = img[0]
-            base_image = doc.extract_image(xref)
-
-            image_bytes = base_image["image"]
-            image_ext = base_image["ext"]  # 'png', 'jpeg', etc.
-
-            # Skip tiny / decorative images
-            if base_image["width"] < 200 or base_image["height"] < 200:
-                continue
-
-            images.append({
-                "page": page_index + 1,
-                "bytes": image_bytes,
-                "mime": f"image/{image_ext}"
-            })
-
-    return images
-
-
-
-def process_pdf(pdf_bytes: bytes, flag: bool) -> str:
-    # Text-based PDF
     reader = PdfReader(BytesIO(pdf_bytes))
     text = []
     for page in reader.pages:
+        
         page_text = page.extract_text()
+        
         if page_text:
             text.append(page_text)
-    
-    if flag:
-        images = extract_images_from_pdf(pdf_bytes)
-        if len(images) > 20: #Just so the ai does not process over 20 images
-            images = images[:20]
-        for img in images:
-            explanation = st.session_state.ai_service.analyze_image(img["bytes"],img["mime"],img["page"])
-            text.append(
-                 f"\n[Image Analysis — Page {img['page']}]\n{analysis}"
-            )
-
+            
     return "\n".join(text)
 
 
