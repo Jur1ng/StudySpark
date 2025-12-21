1f967af8105dc3fdbdfb9dfe4f11ee0f3cd6b196
Jur1ng <iuriisheiman@gmail.com>
2025-12-21 19:22:17 +0000

    Update pdf_processing_service.py

--- StudySpark_Project/services/pdf_processing_service.py
+++ StudySpark_Project/services/pdf_processing_service.py
@@ -1,5 +1,6 @@
 from io import BytesIO
 from pypdf import PdfReader
+from services.ai_service import AIService
 import fitz
 
 
@@ -51,6 +52,7 @@ def process_pdf(pdf_bytes: bytes, flag: bool) -> str:
             )
 
     return "\n".join(text)
+
 
 
 
